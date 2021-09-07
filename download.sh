2f5174228d00304bc1a44e5a8dd402d38fbedc35
sinaenjuni <y2k4388@gmail.com>
2021-09-07 20:20:01 +0900

    upload download.sh

--- download.sh
+++ download.sh
@@ -3,7 +3,15 @@ URL=http://images.cocodataset.org/annotations/annotations_trainval2014.zip
 ZIP_FILE=./data/coco_trainval_annotation.zip
 mkdir -p ./data
 wget -N $URL $ZIP_FILE
-unzip $ZIP_FILE -d ./data/
+unzip $ZIP_FILE -d ./data/coco/annotations
 rm $ZIP_FILE
 
+
+#URL=http://images.cocodataset.org/zips/train2014.zip
+#ZIP_FILE=./data/coco_train_image.zip
+#mkdir -p ./data
+#wget -N $URL $ZIP_FILE
+#unzip $ZIP_FILE -d ./data/coco/images
+#rm $ZIP_FILE
+#
 
